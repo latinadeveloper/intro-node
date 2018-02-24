20533b8fab239bb2c64458db16e7220c8b40079d
Isis Tejeda <isis@isistejeda.com>
2018-02-24 10:41:26 -0600

    Missed:

---
+++ CS602_project_tejeda/product/schema.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const connection = require('../dbConnection.js');
+
+mongoose.Promise = global.Promise;
+
+const productSchema = new Schema({
+	name: {
+		type: String,
+		minlength: 3,
+		required: true
+	},
+	stock: {
+		type: Number,
+		min: 0,
+		required: true
+	},
+	description: String,
+	price: {
+		type: Number,
+		min: 0,
+		required: true
+	}
+});
+
+/*module.exports = {  // taken out since there is only one connection
+	getModel: (connection) => {
+		return connection.model("ProductModel",
+							productSchema);
+	}
+}*/
+
+module.exports = connection.model("ProductModel", productSchema)
